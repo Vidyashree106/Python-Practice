d8852869d9a28ad303a28894fedeb409b12efea5
vidyashree v <vidyateju106@gmail.com>
2026-04-24 21:07:33 +0530

    practiced user defined function with example

--- Functions/.py
+++ Functions/.py
@@ -1,5 +1,11 @@
-                               
-
-
-                             
+def li(numbers):
+    total=0
+    product=1
+    for i in numbers:
+        if i%2==0:
+            product=product*i
+        elif i%2==1:
+            total=total+i
+    print(f'product is:{product} and total is:{total}')
+(li([1,2,3,4,5,6,7,8,9]))                             
 
